19378f7d6b0c86efc4d23f8cd81fb1b397fffba4
destanton <delisestanton@gmail.com>
2016-09-28 15:23:11 -0400

    adding bike classes

---
+++ model_life.py
@@ -0,0 +1,36 @@
+
+
+class Bike:
+
+    def __init__(self, color, wheel_size, frame_material):
+        self.color = color
+        self.wheel_size = wheel_size
+        self.frame_material = frame_material
+
+
+class RoadBike(Bike):
+
+    def __init__(self):
+        self.color = "red"
+        self.wheel_size = 700
+        self.frame_material = "carbon"
+
+
+class MountainBike(Bike):
+
+    def __init__(self):
+        self.color = "grey"
+        self.wheel_size = 29
+        self.frame_material = "aluminum"
+
+
+bike = Bike("red", 29, "aluminum")
+print(bike.color)
+print(bike.wheel_size)
+print(bike.frame_material)
+danielle_bike = bike
+road_bike = RoadBike()
+mountain_bike = MountainBike()
+print(danielle_bike.color)
+print(road_bike.color)
+print(mountain_bike.color)
